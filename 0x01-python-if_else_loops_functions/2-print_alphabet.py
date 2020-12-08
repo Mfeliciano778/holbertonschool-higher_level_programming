ce2404fb4309e85a0461d014f109b221245296e1
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-08 20:14:38 +0000

    Another version of the same code :(

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-a = 97
-while a < 123:
-    print(chr(a), end="")
-    a += 1
+# a = 97
+# while a < 123:
+#     print(chr(a), end="")
+#     a += 1
+for i in range(ord('a'),ord('z')+1):
+    print(chr(i), end="")
