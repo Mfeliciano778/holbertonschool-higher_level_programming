1ce80eec9741eb4a0937d7bb4be5ac30e3369436
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-08 18:47:24 +0000

    Fixed code: added the '-' in front of negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if (abs(number) % 10) > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, number % 10))
-elif (abs(number) % 10) == 0:
-    print("Last digit of {:d} is {:d} and is zero".format(number, number % 10))
-elif (abs(number) % 10) < 6 and not 0:
-    if number > 0:
-        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, number % 10))
-    else:
-        print("Last digit of {:d} is -{:d} and is less than 6 and not 0".format(number, number % 10))
+if number >= 0:
+    lst_dgt = number % 10
 else:
-    print("Not a number")
+    lst_dgt = -(abs(number) % 10)
+
+if abs(lst_dgt) > 5:
+    print("Last digit of", end=" ")
+    print("{:d} is {:d} and is greater than 5".format(number, lst_dgt))
+elif abs(lst_dgt) == 0:
+    print("Last digit of", end=" ")
+    print("{:d} is {:d} and is zero".format(number, lst_dgt))
+else:
+    print("Last digit of", end=" ")
+    print("{:d} is {:d} and is less than 6 and not 0".format(number, lst_dgt))
