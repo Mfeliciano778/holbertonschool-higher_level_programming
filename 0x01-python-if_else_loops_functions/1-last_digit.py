20a340f0ec15e3d829dc609afaa68f576d4f17b6
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-09 01:49:51 +0000

    FIXED THE ZERO

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,7 @@ if lst_dgt > 5:
     print("Last digit of {} is {}\
  and is greater than 5".format(number, lst_dgt))
 elif number % 10 == 0:
-    print("Last digit of {} is {} and is zero".format(number, lst_dgt))
+    print("Last digit of {} is {} and is 0".format(number, lst_dgt))
 elif lst_dgt < 6 and number != 0:
     print("Last digit of {} is {}\
  and is less than 6 and not 0".format(number, lst_dgt))
