5b25be80de1cb15c7688d8fe78f74dbb6509ec84
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-09 01:27:36 +0000

    Changed last if statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,6 @@ if lst_dgt > 5:
  and is greater than 5".format(number, lst_dgt))
 elif number % 10 == 0:
     print("Last digit of {} is {} and is zero".format(number, lst_dgt))
-elif lst_dgt < 6 and not 0:
+elif lst_dgt < 6 and last_dgt is not 0:
     print("Last digit of {} is {}\
  and is less than 6 and not 0".format(number, lst_dgt))
