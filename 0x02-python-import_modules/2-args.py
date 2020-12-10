3a296584340d5e3ea5ac3ef145dd6985ffba91f1
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-10 18:28:42 +0000

    Fixed some code, so it doesnt print argv[0]

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    num = len(argv) - 1
-    if num == 0:
+    num = len(argv)
+    if (num - 1) == 0:
         print("0 arguments.")
 
     elif num == 1:
         print("1 argument:")
-        for i in range(num):
-            print("{:d}: {}".format(i + 1, argv[i]))
+        for i in range(1, num):
+            print("{:d}: {}".format(i, argv[i]))
             i += 1
     else:
-        print("{:d} arguments".format(num))
-        for i in range(num):
-            print("{:d}: {}".format(i + 1, argv[i]))
+        print("{:d} arguments".format(num - 1))
+        for i in range(1, num):
+            print("{:d}: {}".format(i, argv[i]))
             i += 1
