1e6c707eecabebeb0341c48d360d3478c8fb6626
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 22:04:12 +0000

    Changed the way im looping

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for row in matrix:
-            for column in row:
-                print("{:d}".format(column), end=" ")
-            print("\n", end="")
+    for row in matrix:
+        for column in range(0, len(row)):
+            if column == len(row) - 1:
+                print("{:d}".format(row[column]), end="")
+            else:
+                print("{:d}".format(row[column]), end=" "),
+        print()
