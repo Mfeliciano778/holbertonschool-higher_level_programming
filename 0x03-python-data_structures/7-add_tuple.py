27b5fe9cc3332408594d41cbece87cdc3b25b7e8
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 21:21:45 +0000

    Added another check

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    sum_tuple = []
+    a = len(tuple_a)
+    b = len(tuple_b)
+    for index in range(0, 2):
+        if index > a and index > b:
+            sum_tuple.append(0)
+        elif b > a and index > (a - 1):
+            sum_tuple.append(tuple_b[index])
+        elif a > b and index > (b - 1):
+            sum_tuple.append(tuple_a[index])
+        else:
+            sum_tuple.append(tuple_a[index] + tuple_b[index])
+    return tuple(sum_tuple)
