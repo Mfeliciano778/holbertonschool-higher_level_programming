15b2a429547729ac993daf394c2e0374240f928e
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 19:19:13 +0000

    Made pep8 compliant

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-	for i in my_list:
-		if idx > len(my_list) or idx < 0:
-			return my_list
-		elif i == idx:
-			my_list[i] = element
-			return my_list
-	return None
+    for i in my_list:
+        if idx > len(my_list) or idx < 0:
+            return my_list
+        elif i == idx:
+            my_list[i] = element
+            return my_list
+    return None
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-	new_list = my_list.copy()
-	for i in new_list:
-		if idx > len(my_list) or idx < 0:
-			return my_list
-		elif i == idx:
-			new_list[i] = element
-	return new_list
+    new_list = my_list.copy()
+    for i in new_list:
+        if idx > len(my_list) or idx < 0:
+            return my_list
+        elif i == idx:
+            new_list[i] = element
+    return new_list
