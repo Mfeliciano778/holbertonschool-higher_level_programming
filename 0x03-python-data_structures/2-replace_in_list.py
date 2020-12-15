db093fa9aba44de5eacfcb6bee9a715a77e46e87
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 19:43:15 +0000

    Added check for if the list exists

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if my_list:
-    for i in my_list:
-        if idx >= len(my_list) or idx < 0:
-            return my_list
-        elif i == idx:
-            my_list[i] = element
-            return my_list
+        for i in my_list:
+            if idx >= len(my_list) or idx < 0:
+                return my_list
+            elif i == idx:
+                my_list[i] = element
+                return my_list
     return None
