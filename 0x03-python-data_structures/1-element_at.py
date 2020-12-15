3eead030d10cf17156ecb97c309f8491d013e227
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 19:29:25 +0000

    Erased unnecessary code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,5 @@ def element_at(my_list, idx):
     if my_list:
         if idx > len(my_list) or idx < 0:
             return None
-        else:
-            return my_list[idx]
+        return my_list[idx]
     return None
