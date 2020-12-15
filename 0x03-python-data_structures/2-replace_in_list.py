5883eb547f065b33e235614ed59730df8653a66e
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 19:48:21 +0000

    Added = after <

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if my_list:
         for i in my_list:
-            if idx >= len(my_list) or idx < 0:
+            if idx >= len(my_list) or idx <= 0:
                 return my_list
             elif i == idx:
                 my_list[i] = element
                 return my_list
-    return None
