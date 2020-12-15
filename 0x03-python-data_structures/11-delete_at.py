3836225a8a957dd3d8d7e9760b89930f50fd0916
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 22:26:15 +0000

    Added a test case for if the index is out of range

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = my_list.copy()
+    if idx > len(my_list) or idx < 0:
+        return new_list
     del my_list[idx]
     del new_list[idx]
     return new_list
