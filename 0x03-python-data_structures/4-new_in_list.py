9370c2891feb2854d1bb9f841d7422c7ff2e1c10
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 20:05:38 +0000

    got rid of unnecessary code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
-    for i in range(0, new_list):
-        if idx >= len(my_list) or idx < 0:
-            return my_list
-        elif i == idx:
-            new_list[i] = element
+    if idx >= len(my_list) or idx < 0:
+        return my_list
+    new_list[idx] = element
     return new_list
