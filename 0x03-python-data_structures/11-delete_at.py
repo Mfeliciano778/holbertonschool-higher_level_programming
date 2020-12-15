360bf967abe5cc2c9ac7bfea5f3c69fa986ba7e7
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 23:20:24 +0000

    Added and rather than or

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = my_list.copy()
-    if idx > len(my_list) or idx < 0:
-        return new_list
-    del my_list[idx]
-    del new_list[idx]
+    if idx < len(my_list) and idx > 0:
+        print("if")
+        del my_list[idx]
+        del new_list[idx]
     return new_list
