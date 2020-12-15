eb1b3f78f674f845fd1d4da3e4340f99d38c680b
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 19:25:03 +0000

    Got rid of useless print

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,7 +4,6 @@ def element_at(my_list, idx):
         for i in my_list:
             if idx > len(my_list) or idx < 0:
                 return None
-            elif i == idx:
-                print(i)
+            elif my_list[i] == idx:
                 return my_list[idx]
         return None
