021d2a8bbbb7f81117bef06b8b36382c89f17f0b
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 19:50:13 +0000

    Added range function to be able to change the index 0

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if my_list:
-        for i in my_list:
-            if idx >= len(my_list) or idx <= 0:
+        for i in range(0, len(my_list)):
+            if idx >= len(my_list) or idx < 0:
                 return my_list
             elif i == idx:
                 my_list[i] = element
