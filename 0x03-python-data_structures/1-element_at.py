1f191f6827d5ded62ef15076f3a9cecbbc147d4a
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 19:23:05 +0000

    Added check if list exists

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx > len(my_list) or idx < 0:
-            return None
-        elif i == idx:
-            print(i)
-            return my_list[idx]
-    return None
+    if my_list:
+        for i in my_list:
+            if idx > len(my_list) or idx < 0:
+                return None
+            elif i == idx:
+                print(i)
+                return my_list[idx]
+        return None
