e47b9197ecfee2677b428852bc235785c245bbbf
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 22:10:21 +0000

    Added else statement and moved length statement outside if

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    length = len(sentence)
     if sentence:
-        length = len(sentence)
         char = sentence[:1]
+    else:
+        char = None
     return((length, char))
