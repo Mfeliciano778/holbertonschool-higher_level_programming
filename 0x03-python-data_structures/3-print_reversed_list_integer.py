c6b15a77da85a852d6a6f2bd020986a74df67409
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-15 19:53:17 +0000

    Added range function to get the value down to 0

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    idx = 1
-    for i in my_list:
-        print("{}".format(my_list[-idx]))
-        idx += 1
+    if my_list:
+        idx = 1
+        for i in range(0, len(my_list)):
+            print("{}".format(my_list[-(idx)]))
+            idx += 1
