8d115bf894008c86038fc93f150944d9af1f3a96
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-16 00:08:05 +0000

    Added = for check of 0

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = my_list.copy()
-    if idx < len(my_list) and idx > 0:
+    if idx < len(my_list) and idx >= 0:
         del my_list[idx]
         del new_list[idx]
     return new_list
