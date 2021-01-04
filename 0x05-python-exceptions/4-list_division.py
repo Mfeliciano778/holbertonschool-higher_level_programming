2a8c5419f38af681672dfb67f85538e7b2e65d07
Mfeliciano778 <mattfeliciano778@gmail.com>
2021-01-04 18:42:39 +0000

    Added a variable to hold the result and then append

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    sum_list = []
+    sum_list = [0] * list_length
     for i in range(0, list_length):
         try:
-            sum_list.append(my_list_1[i] / my_list_2[i])
+            result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("divison by 0")
-            sum_list.append(0)
-            pass
+            result = 0
         except TypeError:
             print("wrong type")
-            sum_list.append(0)
+            result = 0
         except IndexError:
             print("out of range")
+            result = 0
         finally:
-            pass
+            sum_list[i] = result
     return sum_list
