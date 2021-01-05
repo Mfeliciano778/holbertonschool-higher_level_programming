8ac0559bd95f9a5e4c65fa06958a1aa313a981be
Mfeliciano778 <mattfeliciano778@gmail.com>
2021-01-05 17:51:13 +0000

    Added my_print to print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''Class Square'''
+
+
+class Square:
+    '''Square - defines size'''
+
+    def __init__(self, size=0):
+        '''__init__ - Creates instance
+           _Square_size: size of square'''
+
+        self.__size = size
+
+    def area(self):
+        '''area - returns the area of the square'''
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        '''size - getter for size'''
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''size - setter for size'''
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        '''my_print - prints the square'''
+
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
