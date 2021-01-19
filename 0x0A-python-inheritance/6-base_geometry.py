ae0bcfb2ae7bb7247dd89cd380575dfb5668dab7
Mfeliciano778 <mattfeliciano778@gmail.com>
2021-01-19 18:07:23 +0000

    Added area function that raises an excpetion to class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Empty class BaseGeometery'''
+
+
+class BaseGeometry:
+    '''BaseGeometry - does geometry'''
+
+    def area(self):
+        '''area - raises exception'''
+        raise Exception("area() is not implemented")
