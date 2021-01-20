5fc291a104ee50e9a05a7d29131ae4fcfd4a6ac2
Mfeliciano778 <mattfeliciano778@gmail.com>
2021-01-20 21:53:17 +0000

    Changed the way i'm printing the file's contents

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,4 @@
 def read_file(filename=""):
     '''read_file - reads from a file and prints the contents'''
     with open(filename, encoding='utf-8') as file:
-        for content in file:
-            print(content, end="")
-    print()
+        print(file.read(), end='')
