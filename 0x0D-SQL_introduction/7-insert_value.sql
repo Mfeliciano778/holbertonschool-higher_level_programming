59c55268c88b3c24462cf59fe54c01cb3b0b20d5
Mfeliciano778 <1652@holbertonschool.com>
2021-03-01 20:14:07 +0000

    Fixed error

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- INSERT NEW ROW IN
+-- isnerts new row in
 -- first_table
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, 'Holberton School');
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
