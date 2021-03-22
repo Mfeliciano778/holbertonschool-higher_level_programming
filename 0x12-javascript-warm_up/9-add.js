ea5057eb9bf08f79de007413c924d9431bb1dfd9
Mfeliciano778 <1652@holbertonschool.com>
2021-03-22 19:59:35 +0000

    added some things

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const num1 = process.argv[2];
-const num2 = process.argv[3];
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
 console.log(num1 + num2);
