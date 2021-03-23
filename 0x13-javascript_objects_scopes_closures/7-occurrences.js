c7e74714c41b6741ff8a5705b640fafc88cd1b97
Mfeliciano778 <1652@holbertonschool.com>
2021-03-23 21:25:06 +0000

    Task 7 completed

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-module.exports.nbOccurences = function (list, num) {
+exports.nbOccurences = function (list, searchElement) {
   let occur = 0;
   for (let index = 0; index < list.length; index++) {
-    if (list[index] === num) {
+    if (list[index] === searchElement) {
       occur = occur + 1;
     }
   }
