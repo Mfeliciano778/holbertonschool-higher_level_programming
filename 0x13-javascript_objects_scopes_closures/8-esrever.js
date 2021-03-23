53ff6ba2959a7e0a17ce1c7f1a438b61af7ac0cd
Mfeliciano778 <1652@holbertonschool.com>
2021-03-23 22:02:41 +0000

    Got rid of brackets

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const newlist = [];
   for (let index = list.length; index > 0; index--) {
-    newlist.push([list[index - 1]]);
+    newlist.push(list[index - 1]);
   }
   return newlist;
 };
