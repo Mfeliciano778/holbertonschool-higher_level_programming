712292804ad6b3690d2bc812a1c3e5a1af5920ea
Mfeliciano778 <1652@holbertonschool.com>
2021-03-23 21:51:26 +0000

    Fixed some more code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,13 @@ const square = require('./5-square');
 
 class Square extends square {
   charPrint (c) {
+    let char = c;
     if (c === undefined || c === null) {
-      super.print();
+      char = 'X';
     } else {
       for (let row = 0; row < this.size; row++) {
         for (let column = 0; column < this.size; column++) {
-          process.stdout.write('C');
+          process.stdout.write(char);
         }
         console.log('');
       }
