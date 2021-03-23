2e552fab44b18315bb90ebe4d108b160840bdd19
Mfeliciano778 <1652@holbertonschool.com>
2021-03-23 21:57:50 +0000

    Hopefully this works

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
-const square = require('./5-square');
+const Rectangle = require('./5-square');
 
-class Square extends square {
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
-    let char = c;
-    if (c === undefined || c === null) {
-      char = 'X';
+    if (c === undefined) {
+      c = 'X';
     }
-    for (let row = 0; row < this.size; row++) {
-      for (let column = 0; column < this.size; column++) {
-        process.stdout.write(char);
+    for (let row = 0; row < this.height; row++) {
+      for (let column = 0; column < this.width; column++) {
+        process.stdout.write(c);
       }
       console.log('');
     }
