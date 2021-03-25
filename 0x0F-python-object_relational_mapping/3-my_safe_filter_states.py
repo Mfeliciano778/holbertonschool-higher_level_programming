c0b1333de50e6693f0e66d893b517e759c890d62
Mfeliciano778 <1652@holbertonschool.com>
2021-03-25 19:25:46 +0000

    Task 3 completed

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Task 3'''
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    db.close()
