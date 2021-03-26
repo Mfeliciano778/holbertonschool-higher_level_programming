c886a9e1d739c889a7125b961c118ea5d518e382
Mfeliciano778 <1652@holbertonschool.com>
2021-03-26 21:33:04 +0000

    Task 11 completed

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+
+from sqlalchemy.orm.session import Session
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    lou_state = session.query(State.name, State.id).all()
+    popeyes = State()
+    popeyes.id = len(lou_state) + 1
+    popeyes.name = 'Louisiana'
+    print(popeyes.id)
+    session.add(popeyes)
+    session.commit()
+    session.close()
