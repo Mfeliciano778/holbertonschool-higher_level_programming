e37d78cae02a24af94c446cb4b0aed965528436d
Mfeliciano778 <1652@holbertonschool.com>
2021-04-08 23:55:47 +0000

    Changed the url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as r:
         body = r.read()
     print("Body response:")
-    print("     - type: {}".format(type(body)))
-    print("     - content: {}".format(body))
-    print("     - utf8 content: {}".format(body.decode('utf-8')))
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    print("    - utf8 content: {}".format(body.decode('utf-8')))
