8636107d8b7c7f599e0dccdfb2fc9b79de9dc32f
Mfeliciano778 <1652@holbertonschool.com>
2021-04-08 23:46:19 +0000

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/projects/300') as r:
+        body = r.read()
+    print("Body Response:")
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    print("    - utf8 content: {}".format(body.decode('utf-8')))
