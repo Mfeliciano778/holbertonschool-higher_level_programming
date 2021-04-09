6eb69ee03b093002d4c03a6dd7347ce78787f971
Mfeliciano778 <1652@holbertonschool.com>
2021-04-09 04:56:14 +0000

    Made pep8 compliant

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,17 +5,17 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if len(argv) < 1:
+    if len(argv) < 2:
         q = ""
     else:
         q = argv[1]
-    data = {"q": q}
+    data = {'q': q}
     try:
-        body = requests.post(url)
-        json_rep = body.json()
-        if json_rep != "{}":
-            print("[{}] {}".format(json_rep.get("id"), json_rep.get("name")))
-        else:
-            print("No result")
+        body = requests.post(url, data).json()
     except:
         print("Not a valid JSON")
+    else:
+        if body != "{}":
+            print("[{}] {}".format(body.get("id"), body.get("name")))
+        else:
+            print("No result")
