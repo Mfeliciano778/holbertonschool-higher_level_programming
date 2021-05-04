ce099aa57ac36e14a8f030e87d97e91c0c2ad529
Mfeliciano778 <1652@holbertonschool.com>
2021-05-04 17:21:11 +0000

    Task 0: made semistandard compliant

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 const filename = process.argv[2];
 
 fs.readFile(filename, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    console.log(data)
-})
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
